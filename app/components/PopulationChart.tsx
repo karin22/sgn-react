4f1182583982b44a4c90a9c675bf4193b4508f57
Karin <karin.piml@gmail.com>
2023-12-30 00:15:35 +0700

    add country flags

--- app/components/PopulationChart.tsx
+++ app/components/PopulationChart.tsx
@@ -7,6 +7,7 @@ import { RangeSlider } from "flowbite-react";
 interface Country {
   year: number;
   population: number;
+  flag: string;
   name: string;
 }
 interface Population {
@@ -15,12 +16,12 @@ interface Population {
   country: Country[];
 }
 const PopulationChart = () => {
-  // Your existing code here
   const [population, setPopulation] = useState<Population[]>([]);
   const [year, setYear] = useState(1950);
   const [startYear, setStartYear] = useState(1950);
   const [endYear, setEndYear] = useState(2018);
   const [isPlay, setIsPlay] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const [sequenceTimer, setSequenceTimer] = useState<any>(undefined);
   useEffect(() => {
@@ -32,12 +33,28 @@ const PopulationChart = () => {
   }, [population, year]);
 
   const getCountry = useMemo(() => {
-    return getPopulationYear[0]?.country.filter((_, ind) => ind < 12);
+    const countryRank = getPopulationYear[0]?.country.filter(
+      (_, ind) => ind < 12
+    );
+    return countryRank;
   }, [getPopulationYear]);
 
   const getAllYears = useMemo(() => {
     return population.map((x) => x.year);
   }, [population]);
+
+  const fetchFlag = async (name: string) => {
+    const res = await fetch(
+      `https://restcountries.com/v3.1/name/${name}?fullText=true`,
+      {
+        method: "GET",
+      }
+    );
+    const _data = await res.json();
+    if (_data) {
+      return _data[0].flags.svg;
+    }
+  };
 
   const fetchData = async () => {
     const res = await fetch(
@@ -47,7 +64,22 @@ const PopulationChart = () => {
       }
     );
     const _data = await res.json();
-    const populationResult: Population[] = _data.data;
+    const populations: Population[] = _data.data;
+
+    populations.forEach((data) => {
+      data.country = data.country.map((c, ind) => {
+        c.flag = "https://flagcdn.com/fr.svg"; // Default flags
+
+        if (ind < 12) {
+          fetchFlag(c.name).then((res) => (c.flag = res));
+        }
+        return c;
+      });
+
+      return data;
+    });
+
+    const populationResult: Population[] = populations;
     setPopulation(populationResult);
 
     const startYearINData = populationResult[0].year;
@@ -55,11 +87,16 @@ const PopulationChart = () => {
 
     setStartYear(startYearINData);
     setEndYear(endYearINData);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
   };
 
   const formatNumber = (number = 0) => {
     return new Intl.NumberFormat("th").format(number);
   };
+
   const getSubtitle = () => {
     return `<span style="font-size: 60px; text-align:end;">${year}</span>
           <br>
@@ -67,6 +104,13 @@ const PopulationChart = () => {
               Total: <b>: ${formatNumber(getPopulationYear[0]?.total)}</b> 
           </span>`;
   };
+
+  const getFlagsImg = (r: any) => {
+    return `<img class="w-6 h-6 rounded-full object-cover" src="${
+      getCountry?.find((x) => x.name === r)?.flag
+    }" />`;
+  };
+
   const chartOptions = useMemo(() => {
     return {
       chart: {
@@ -99,6 +143,7 @@ const PopulationChart = () => {
           },
         },
       },
+
       yAxis: {
         opposite: true,
         tickPixelInterval: 300,
@@ -106,6 +151,7 @@ const PopulationChart = () => {
           text: null,
         },
       },
+
       subtitle: {
         useHTML: true,
         text: getSubtitle(),
@@ -116,7 +162,25 @@ const PopulationChart = () => {
         align: "right",
         verticalAlign: "bottom",
       },
+
+      tooltip: {
+        outside: true,
+      },
       plotOptions: {
+        bar: {
+          dataLabels: {
+            enabled: true,
+            useHTML: true,
+            style: {
+              left: 0,
+            },
+            formatter: function (this: Highcharts.PointLabelObject) {
+              return `
+              <div class="flex items-center gap-2 absolute -left-8 -top-3">
+              ${getFlagsImg(this.key)} ${formatNumber(Number(this.y))}</div>`;
+            },
+          },
+        },
         series: {
           animation: true,
           groupPadding: 0,
@@ -135,12 +199,6 @@ const PopulationChart = () => {
       },
       series: [
         {
-          dataLabels: {
-            enabled: true,
-            formatter: function (this: Highcharts.PointLabelObject) {
-              return `${formatNumber(Number(this.y))}`;
-            },
-          },
           data: getCountry?.map((x) => [x.name, x.population]),
         },
       ],
@@ -182,48 +240,52 @@ const PopulationChart = () => {
   };
   return (
     <>
-      <HighchartsReact highcharts={Highcharts} options={chartOptions} />
+      {!loading && (
+        <div>
+          <HighchartsReact highcharts={Highcharts} options={chartOptions} />
 
-      <div className="w-full flex gap-6 max-w-[1000px] mx-auto">
-        <button
-          id="play-pause-button"
-          className="grid place-items-center w-20"
-          title="play"
-          onClick={!isPlay ? onPlay : onPause}
-        >
-          {!isPlay ? <FaPlay /> : <FaPause />}
-        </button>
-        <div className="flex-1 overflow-auto">
-          <RangeSlider
-            onChange={onChangeRange}
-            className="w-full pl-4"
-            id="default-range"
-            value={year}
-            min={startYear}
-            max={endYear}
-          />
-          <div className="flex text-[10px] relative gap-1 border-t ml-6">
-            {getAllYears.map((x, ind) => {
-              return (
-                <div
-                  className={
-                    ind % 4 === 0 ? "rang-year" : "rang-year highlight"
-                  }
-                  key={x}
-                >
-                  {ind % 4 === 0 ? (
-                    <span className="text-year absolute top-[10px] -left-[10px]">
-                      {x}
-                    </span>
-                  ) : (
-                    ""
-                  )}
-                </div>
-              );
-            })}
+          <div className="w-full flex gap-6 max-w-[1000px] mx-auto">
+            <button
+              id="play-pause-button"
+              className="grid place-items-center w-20"
+              title="play"
+              onClick={!isPlay ? onPlay : onPause}
+            >
+              {!isPlay ? <FaPlay /> : <FaPause />}
+            </button>
+            <div className="flex-1 overflow-auto">
+              <RangeSlider
+                onChange={onChangeRange}
+                className="w-full pl-4"
+                id="default-range"
+                value={year}
+                min={startYear}
+                max={endYear}
+              />
+              <div className="flex text-[10px] relative gap-1 border-t ml-6">
+                {getAllYears.map((x, ind) => {
+                  return (
+                    <div
+                      className={
+                        ind % 4 === 0 ? "rang-year" : "rang-year highlight"
+                      }
+                      key={x}
+                    >
+                      {ind % 4 === 0 ? (
+                        <span className="text-year absolute top-[10px] -left-[10px]">
+                          {x}
+                        </span>
+                      ) : (
+                        ""
+                      )}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
